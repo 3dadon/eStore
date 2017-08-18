9410fa96d3d763aeb3cba4b8da966b2220ba13bf
3dadon <paul.henri.larbat@gmail.com>
2017-08-18 13:00:36 +0200

    creation servlet CreationClient et jsp creerclient.jsp

---
+++ src/com/eStore/beans/Client.java
@@ -0,0 +1,5 @@
+package com.eStore.beans;
+
+public class Client {
+
+}
---
+++ src/com/eStore/servlets/CreationClient.java
@@ -0,0 +1,19 @@
+package com.eStore.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class CreationClient extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		System.out.println("Entrée dans la servlet ...");
+		this.getServletContext().getRequestDispatcher("/creerClient.jsp").forward(req, resp);
+	}
+}
